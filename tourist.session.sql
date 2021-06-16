37b6133b5b301e8c6692478e9ec9c93525a968ee
Denis Kominarets <kominaretsdenis@gmail.com>
2021-06-16 07:13:05 +0600

    1-st task DB

---
+++ tourist.session.sql
@@ -0,0 +1,21 @@
+-- INSERT INTO outter_flights(from_country, to_country, flight_type, company, neighbors)
+--  VALUES('Northway', 'Sweden', 'passenger', 'NorthCold Fly', 0);
+
+-- select * from outter_flights;
+-- select * from inner_flights;
+-- select * from inner_flights WHere id > 10;
+-- select * from inner_flights WHere to_destination = 'The USA';
+-- select * from inner_flights WHere  quantity > 150;
+-- select company from outter_flights WHere  flight_type = 'passenger';
+-- select * from outter_flights WHere  id < 7;
+-- select * from outter_flights WHere  flight_type = 'cargo';
+-- select * from outter_flights WHere  neighbors > 3;
+-- select * from outter_flights WHere  neighbors < 3 AND flight_type = 'passenger';
+-- select company, to_country from outter_flights;
+
+
+
+
+
+
+
